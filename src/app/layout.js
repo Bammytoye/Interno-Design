171d6615b27baa802130062db1e4fbce3f3887a6
bammytoye <bamigbalatoyese@gmail.com>
2025-02-09 09:04:01 +0100

    installMotion

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Transition from "@/transition";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+          <Transition> 
           {children}
+          </Transition>
         <Footer />  
       </body>
     </html>
